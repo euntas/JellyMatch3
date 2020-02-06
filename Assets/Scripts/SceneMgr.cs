b053a6412dd2d1586936f58c7e10175682eb709e
euntas <tavstaus@hanmail.net>
2020-02-06 16:29:06 +0900

    로딩 기능 구현

---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMgr : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void OpenPlayScene()
+    {
+        LoadingSceneManager.LoadScene("PlayScene");
+    }
+
+    public void OpenToolScene()
+    {
+        // TODO. tool 씬이 아직 없으므로 지금은 스타트 씬 로드
+        LoadingSceneManager.LoadScene("StartScene");
+    }
+}
